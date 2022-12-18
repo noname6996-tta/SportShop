cf70973cbdd078faaa3d5079a252809e487c32df
Trần  Thế Anh <theanh682001@gmail.com>
2022-12-18 15:07:50 +0700

    first_commit

--- app/src/main/java/com/example/sportshopapplication/view/fragment/CartFragment.kt
+++ app/src/main/java/com/example/sportshopapplication/view/fragment/CartFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sportshopapplication.view.fragment
 
+import android.widget.Toast
 import com.example.sportshopapplication.databinding.FragmentCartBinding
 import com.example.sportshopapplication.databinding.FragmentHomeBinding
 import com.proxglobal.worlcupapp.base.BaseFragment
@@ -7,5 +8,9 @@ import com.proxglobal.worlcupapp.base.BaseFragment
 class CartFragment: BaseFragment<FragmentCartBinding>() {
     override fun getDataBinding(): FragmentCartBinding {
         return FragmentCartBinding.inflate(layoutInflater)
+    }
+
+    override fun addEvent() {
+        super.addEvent()
     }
 }
