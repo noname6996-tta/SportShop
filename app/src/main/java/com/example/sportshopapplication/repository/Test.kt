7f7c1d9c8c1b48ec6ad38d206f8ee9f42c4df3bb
manhhlunn <manhhlunn@gmail.com>
2022-12-19 13:59:38 +0700

    aabc

--- app/src/main/java/com/example/sportshopapplication/repository/Test.kt
+++ app/src/main/java/com/example/sportshopapplication/repository/Test.kt
@@ -3,5 +3,7 @@ package com.example.sportshopapplication.repository
 import java.util.concurrent.ConcurrentLinkedQueue
 
 class Test {
+    val b = ConcurrentLinkedQueue<String>()
+    val dr= ConcurrentLinkedQueue<String>()
     val a = ConcurrentLinkedQueue<String>()
 }
